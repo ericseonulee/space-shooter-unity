1f224b72ee4b25ab8c732e76fcda7b20ca5d2574
Eric Lee <szl0094@auburn.edu>
2022-08-10 15:28:25 -0500

    new main menu and ui sfx

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -5,8 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource mySFX;
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
+    public AudioClip swishoutSFX;
+
     public void LoadGame() {
-        //load the game scene
+        StartCoroutine(LoadGameRoutine());
+    }
+
+    public void ExitGame() {
+        StartCoroutine(ExitGameRoutine());
+    }
+
+    public void HoverSound() {
+        mySFX.PlayOneShot(hoverSFX, 0.5f);
+    }
+
+    public void ClickSound() {
+        mySFX.PlayOneShot(clickSFX, 0.5f);
+    }
+
+    public void SwishoutSound() {
+        mySFX.PlayOneShot(swishoutSFX, 0.5f);
+    }
+
+    IEnumerator LoadGameRoutine() {
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(1); //game scene
+    }
+
+    IEnumerator ExitGameRoutine() {
+        yield return new WaitForSeconds(2f);
+        Application.Quit();
     }
 }
